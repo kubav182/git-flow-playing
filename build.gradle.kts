c6860507aeca0175a11f1b9b56d57b950ee9bda7
Jakub Venglar <venglar.jakub@gmail.com>
2020-03-12 16:25:22 +0100

    m

--- build.gradle.kts
+++ build.gradle.kts
@@ -253,15 +253,18 @@ tasks.register("hello") {
         //println("git diff-index HEAD".runCommand().isNullOrEmpty())
 
         println("1")
-        println("git format-patch master --stdout".runCommand())
-        println("2")
-        println("git checkout master".runCommand())
-        println("3")
-        println("git apply crazy.patch --check".runCommand())
-        println("4")
-        //println("rm crazy.patch".runCommand())
-        println("5")
-        println("git checkout develop".runCommand())
+        val patch = "git format-patch master --stdout".runCommand()
+        if (patch != null) {
+            File("crazy.patch").writeText(patch)
+            println("2")
+            println("git checkout master".runCommand())
+            println("3")
+            println("git apply crazy.patch --check".runCommand())
+            println("4")
+            //println("rm crazy.patch".runCommand())
+            println("5")
+            println("git checkout develop".runCommand())
+        }
 
     }
 }
